fb68a9012edc27fab10e98dc6a70f523014abe85
ccarrions <cristiancarrion00@gmail.com>
2022-10-16 22:05:11 -0300

    Versión funcional
    Esta versión fue restaurado de una versión antigua y se agregó, cámaras para ambos jugadores y un nivel sandbox.

---
+++ scripts/Game.gd
@@ -0,0 +1,25 @@
+extends Node
+
+onready var players:= {
+	"1": {
+		viewport = $GridContainer/ViewportContainer/Viewport,
+		camera = $GridContainer/ViewportContainer/Viewport/Camera2D,
+		player = $GridContainer/ViewportContainer/Viewport/Level/Player1,
+		
+	},
+	"2": {
+		viewport = $GridContainer/ViewportContainer2/Viewport,
+		camera = $GridContainer/ViewportContainer2/Viewport/Camera2D,
+		player = $GridContainer/ViewportContainer/Viewport/Level/Player2,
+	
+	}
+}
+
+
+func _ready() -> void:
+	players["2"].viewport.world_2d = players["1"].viewport.world_2d
+	for node in players.values():
+		var remote_transform = RemoteTransform2D.new()
+		remote_transform.remote_path = node.camera.get_path()
+		node.player.add_child(remote_transform)
+
