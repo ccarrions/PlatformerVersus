fb003530f108a5c20a0a409992d14673bde66889
cristian <cristiancarrion00@gmail.com>
2022-10-20 22:14:23 -0300

    minor fixes

--- Character/character.gd
+++ Character/character.gd
@@ -27,28 +27,16 @@ func _ready():
 
 func _physics_process(delta):
 
-	var move_input = Input.get_axis(controls.move_left, controls.move_right)
-	velocity = move_and_slide(velocity, Vector2.UP)
-	velocity.x = move_toward(velocity.x, move_input*SPEED, ACCELERATION*delta)
-	velocity.y += GRAVITY
-	if is_on_floor() and Input.is_action_just_pressed(controls.jump):
-		velocity.y = JUMP_SPEED
 	
-	if is_on_wall() and Input.is_action_just_pressed(controls.move_left):
-		velocity.y = -JUMP_WALL
-		velocity.x = -WALL_JUMP
-	if is_on_wall() and Input.is_action_just_pressed(controls.move_right):
-		velocity.y = -JUMP_WALL
-		velocity.x = WALL_JUMP
 			
 	# Wall slide
 	if is_on_wall() and velocity.y > 30:
 		velocity.y = 30	
 		
 	#Animations
-	if Input.is_action_just_pressed("p1_move_right") and not Input.is_action_just_pressed("p1_move_left"):
+	if Input.is_action_just_pressed(controls.move_right) and not Input.is_action_just_pressed(controls.move_left):
 		pivot.scale.x = 1
-	if Input.is_action_just_pressed("p1_move_left") and not Input.is_action_just_pressed("p1_move_right"):
+	if Input.is_action_just_pressed(controls.move_left) and not Input.is_action_just_pressed(controls.move_right):
 		pivot.scale.x = -1
 	if is_on_floor():
 		if abs(velocity.x) > 10:
